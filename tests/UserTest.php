232c42ec34b077955c3e6b6042c4ecdb512c3c8d
Ruzal Shrestha <71171919+Ruzal-create@users.noreply.github.com>
2023-04-20 09:37:42 +0545

    Update UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase {
   public function testAdd() {
-//     require_once('../index.php');
+    require_once('index.php');
     $this->assertEquals(5, add(2, 3));
     $this->assertEquals(10, add(5, 5));
     $this->assertEquals(-1, add(0, -1));
